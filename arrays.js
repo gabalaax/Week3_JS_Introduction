49d9a1743919d34de4c7d08fa1f0c4171c3ab6d9
gabalaax <rahmaabdikaniwaxbarasho@gmail.com>
2023-02-24 21:29:33 +0300

    asjdgvf

--- arrays.js
+++ arrays.js
@@ -58,20 +58,21 @@ let inventory = [
     { id: 49, car_make: "Chrysler", car_model: "Sebring", car_year: 1996 },
     { id: 50, car_make: "Lincoln", car_model: "Town Car", car_year: 1999 }
   ];
-  
+
+  //
   // Example Challenge:
 
   // The dealer wants to know how many "Mazda" cars they have. / Waxay rabaan inay ogaadaan meeqo gaari oo "Mazda" ah ayaa yaalo.
   
-       let mazdaCars = []
-       
-      for (let i = 0; i < inventory.length; i++) {
-           if (inventory[i].car_make == "Mazda"){
-               mazdaCars.push(inventory[i].car_make)
-           }
-       }
-    
-   console.log("We have ", mazdaCars.length,  "Mazda Cars")
+  //     let mazdaCars = []
+  //     
+  //    for (let i = 0; i < inventory.length; i++) {
+  //         if (inventory[i].car_make == "Mazda"){
+  //             mazdaCars.push(inventory[i].car_make)
+  //         }
+  //     }
+  //  
+  // console.log("We have ", mazdaCars.length,  "Mazda Cars")
 
   
   // ==== Challenge 1 ====
@@ -80,32 +81,52 @@ let inventory = [
   // Ma xasuustaan ID-ga gaariga 33 aad, ka caawi inay ogaadaan ID-ga gaariga 33aad. Waa inaa soo bandhigto sanadka gaariga, noocuu yahay (make) iyo modelka gaariga. Habkaan usoo bandhig:
 
   // console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*`);
+      
+      
+        console.log(inventory[32] )
 
-
-  
   // ==== Challenge 2 ====
   // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
-
   // Waxay rabaan inay ogaadaan macluumaadka gaariga ugu dambeeyay. Waxaa ka mid ah inay noocuu yahay (make) iyo modelka gaariga ugu dambeeyay.
 
   
   // ==== Challenge 3 ====
   // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-  
   // Dadka qaabilsan xayaysiinta ayaa rabo in gawaarida loo soo bandhigo xarfaha habkey iskugu xigaan (alphabetically) si ay website-ka u galiyaan. Magacyada gawaawida oo dhan isku habee si A-Z ah kadibna console.log ku samee.
 
-
+   
+   console.log(inventory.sort(function (a, b)   {
+    return b - a
+   }))
   // ==== Challenge 4 ====
   // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-  
   // Dadka qaabilsan xisaabinta ayaa rabo inay ogaadaan sanadyada gawaarida oo dhan. Array cusub samee, kadibna ku shub sanadyada gawaarida oo dhan kadibna console.log ku samee.
-  
+  const car_year = [] 
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_year) {
+      car_year.push(inventory[i].car_year)
+    }
+  }
+   console.log(car_year)
   // ==== Challenge 5 ====
   // The car lot manager needs to find out how many cars are older than the year 2000. Find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
-
+  const oldCars = []
+   for (let i = 0;i < inventory.length; i++) {
+    if (inventory[i].car_year < 2000) {
+        oldCars.push(inventory[i].car_year)
+    }
+   }
+   console.log(oldCars)
   // Qofka maamulo ganacsiga ayaa rabo inuu ogaado inta gaari oo ka horeysay sanadkii 2000. Isticmaal array 'oldCars', kuna shub gawaarida ka horeysay 2000. Kadib console log ku samee.
   
   // ==== Challenge 6 ====
   // A buyer is interested in seeing only BMW and Audi cars within the inventory. Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
 
   // Qof rabo inuu gaari gato ayaa rabo inuu ogaado inta BMW iyo Audi yaalo. Array 'BMWAndAudi' la dhaho ku shub dhamaan gawaarida BMW iyo Audi. Kadib adigoo isticmaalaayo JSON.stringify() console.log ku samee.
+  const BMWAndAudi = [] 
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_make === "BMW", "Audi") {
+      BMWAndAudi.push(inventory[i].car_make)
+    }
+  }
+  console.log(BMWAndAudi)
